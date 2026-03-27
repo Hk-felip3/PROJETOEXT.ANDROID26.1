59868ff9b4962f825692c560fa21b57704cb04b4
Gaia28 <matheusgaia33@gmail.com>
2026-03-27 16:12:04 -0300

    template para tela de gabaritos

---
+++ mobile-app/app/(tabs)/gabaritos.tsx
@@ -0,0 +1,43 @@
+import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import Header from '@/components/header';
+import { HeaderBackButton } from '@react-navigation/elements';
+import { useNavigation } from '@react-navigation/native';
+
+
+export default function HomeScreen() {
+    const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View >
+        <Header
+        title="ScanGrab"
+        brand={<HeaderBackButton onPress={() => navigation.goBack()} />}
+        rightAction={
+            <TouchableOpacity style={styles.rightAction}>
+              <Text style={styles.rightActionText}>Novo</Text>
+            </TouchableOpacity>
+          }
+        />
+        </View>
+        <Text>Explore Screen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  rightAction: {
+    marginLeft: 'auto',
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    borderRadius: 8,
+    backgroundColor: '#111827',
+  },
+  rightActionText: {
+    color: '#ffffff',
+    fontWeight: '600',
+  },
+});
