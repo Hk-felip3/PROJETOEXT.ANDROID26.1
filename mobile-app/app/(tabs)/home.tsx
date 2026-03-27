a7b9c8306c564936901faffa0f1b65a3c10b3433
Gaia28 <matheusgaia33@gmail.com>
2026-03-27 16:12:16 -0300

    template de home

--- mobile-app/app/(tabs)/home.tsx
+++ mobile-app/app/(tabs)/home.tsx
@@ -1,12 +1,21 @@
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View,Text } from 'react-native';
 import Header from '@/components/header';
-
+import { StatusBar } from 'expo-status-bar';
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <View >
-        <Header title="ScanGrab"/>
+        <Header
+        title="ScanGrab"
+        brand={ <View style={{ width: 32, height: 32, backgroundColor: 'black', borderRadius: 8 }} />}
+        />
         </View>
+        <Text>Home Screen</Text>
+          <StatusBar
+          style="auto"
+          hidden={true}
+          />
+
     </SafeAreaView>
   );
 }
